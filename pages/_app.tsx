48cf5a983e5d3bf5bd42c3de5665fe4ca205430a
martinmortier <martin@DESKTOP-31158NL.localdomain>
2021-10-16 14:05:29 +0200

    Add Mui component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { CacheProvider, EmotionCache } from "@emotion/react"
+import createEmotionCache from "../mui-config/createEmotionCache"
+import theme from "../mui-config/theme"
+const clientSideEmotionCache = createEmotionCache()
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      {/* <ThemeProvider theme={theme}> */}
+      <CssBaseline />
+      <Component {...pageProps} />
+      {/* </ThemeProvider> */}
+    </CacheProvider>
+  )
 }
 export default MyApp
