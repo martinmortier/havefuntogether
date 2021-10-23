c1bf38b68c4f19460b08df88d22147ff98e6a58b
martinmortier <martin@DESKTOP-31158NL.localdomain>
2021-10-23 10:31:56 +0200

    Refactor Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,7 @@
 import type { NextPage } from "next"
 import Head from "next/head"
-import { useUser } from "@auth0/nextjs-auth0"
-import React, { useEffect } from "react"
 import Home from "../components/Home/Home"
-export function Profile() {
-  const { user, error, isLoading } = useUser()
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>{error.message}</div>
-
-  return (
-    user && (
-      <div>
-        {/* <img src={user.picture} alt={user.name} /> */}
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
-  )
-}
 const Index: NextPage = () => {
   return (
     <div>
