666ab2338c16ef92467ff09c69581c6fe982de65
martinmortier <martin@DESKTOP-31158NL.localdomain>
2021-11-28 22:42:14 +0100

    Refactor Chat component

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useMemo, useState } from "react"
 import { io } from "socket.io-client"
 import { FormEvent, useEffect } from "react"
 import { useUser } from "@auth0/nextjs-auth0"
@@ -8,23 +8,24 @@ interface Message {
   nickname: string
   content: string
 }
-
+const url = process.env.NODE_ENV === "test" ? "" : "http://localhost:3001/"
+const socket = io(url)
+const MAXIMUM_LENGTH = 7
 const Chat = (): JSX.Element => {
   const { user } = useUser()
   const [messages, setMessages] = useState<Array<Message>>([])
   const [newMessage, setNewMessage] = useState<string>("")
-  const url = process.env.NODE_ENV === "test" ? "" : "http://localhost:3001/"
-  const socket = io(url)
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault()
-    if (user && user.name && newMessage) {
+    if (user?.name && newMessage) {
       const message: Message = {
         nickname: user.name,
         content: newMessage,
       }
 
       setMessages(messages.concat(message))
+      //TODO: Test socket
       socket.emit("chat message", message)
       socketDisconnect()
       setNewMessage("")
@@ -45,21 +46,19 @@ const Chat = (): JSX.Element => {
     socket.disconnect
   }
   useEffect(() => {
-    const arraySlice: Message[] = sliceFirstElement(messages, 7)
+    const arraySlice: Message[] = sliceFirstElement(messages, MAXIMUM_LENGTH)
     if (arraySlice.length > 0) setMessages(arraySlice)
     return () => socketDisconnect()
   }, [messages])
-
   return (
     <div>
       <h1>Chat !</h1>
-      {messages.length > 0 &&
-        messages.map((msg: Message, index) => (
-          <div key={index}>
-            <p>{msg.nickname}</p>
-            <p>Message: {msg.content}</p>
-          </div>
-        ))}
+      {messages.map((msg: Message, index: number) => (
+        <div key={index}>
+          <p>{msg.nickname}</p>
+          <p>Message: {msg.content}</p>
+        </div>
+      ))}
       <form onSubmit={handleSubmit}>
         <TextField
           type="text"
